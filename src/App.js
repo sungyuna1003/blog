43310168f4a7f4b8556c0ea92a5f9813da9eb1be
sungyuna1003 <sungyuna1003@gmail.com>
2021-12-06 14:55:56 +0900

    state practice

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+// import logo from "./logo.svg";
 
 function App() {
+  let [글제목, 글제목변경] = useState(["식당추천"], ["카페추천"]);
+
+  // let posts = "restaurants";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div>Yuna's blog</div>
+      </div>
+      <div className="list">
+        <h3>{글제목[0]}</h3>
+        <p>2월 17일 발행</p>
+        <hr />
+      </div>
+      <div className="list">
+        <h3>{글제목[0]}</h3>
+        <p>3월 17일 발행</p>
+        <hr />
+      </div>
+      <div className="list">
+        <h3>{글제목[0]}</h3>
+        <p>4월 17일 발행</p>
+        <hr />
+      </div>
     </div>
   );
 }
